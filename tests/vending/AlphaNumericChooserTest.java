6ca059b1dab4d0c328401629fc583414305fc43b
DimaSanKiev <dmytro.burdyga@gmail.com>
2016-08-08 12:55:37 +0300

    assertions with messages

---
+++ tests/vending/AlphaNumericChooserTest.java
@@ -0,0 +1,25 @@
+package vending;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AlphaNumericChooserTest {
+
+    private AlphaNumericChooser chooser;
+
+    @Before
+    public void setUp() throws Exception {
+        chooser = new AlphaNumericChooser(26, 10);
+    }
+
+    @Test
+    public void validInputReturnsProperLocation() throws Exception, InvalidLocationException {
+        AlphaNumericChooser.Location loc = chooser.locationFromInput("B4");
+
+        assertEquals("proper row", 1, loc.getRow());
+        assertEquals("proper column", 3, loc.getColumn());
+    }
+
+}
